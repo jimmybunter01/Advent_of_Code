2c8c44c87f241fb81a0d08c697b93f99c14f37bd
jimmybunter01 <65281539+jimmybunter01@users.noreply.github.com>
2023-12-12 19:59:55 +0000

    Completed Day 1 Part 1, just needed to fix some bugs not caught with the example input!

--- 2023/advent_of_code_2023_rs/src/main.rs
+++ 2023/advent_of_code_2023_rs/src/main.rs
@@ -36,8 +36,10 @@ fn parse_input(input: Vec<Vec<char>>) {
             if input[i][j as usize].is_digit(10) {
                 let current_digit = input[i][j as usize];
                 digit_parts.push(current_digit);
-
-                println!("{}", current_digit);
+                
+                if digit_parts == vec!('2', '5', '8') {
+                    println!();
+                }
 
                 if check_valid_digit(i as i16, j as i16, &input) == true {
                     let mut rest_of_digit: Vec<char> = get_rest_of_digit(i, j as usize, &input);
@@ -45,13 +47,11 @@ fn parse_input(input: Vec<Vec<char>>) {
                     
                     digit_parts.append(&mut rest_of_digit);
 
-                    println!("{} -> {}", j, j as usize + j_add_factor);
-
                     let digit_string: String = digit_parts.iter().collect();
 
                     println!("{}\r\n", digit_string);
 
-                    let digit: u16 = digit_string.parse().unwrap();
+                    let digit: u64 = digit_string.parse().unwrap();
 
                     digit_total += digit;
                 }
@@ -75,7 +75,7 @@ fn check_valid_digit(i: i16, j: i16, input: &Vec<Vec<char>>) -> bool {
     let input_length: usize = input.len();
     let line_length: usize = input[0].len();
 
-    let possible_symbols: [char; 4] = ['*', '#', '+', '$'];
+    let possible_symbols: [char; 10] = ['*', '#', '+', '$', '@', '/', '-', '&', '%', '='];
 
     if (i == 0) & (j == 0) {
         let positions_to_check: [i16; 2] = [0, 1];
@@ -172,7 +172,26 @@ fn check_valid_digit(i: i16, j: i16, input: &Vec<Vec<char>>) -> bool {
             }
         }
         false
-    } 
+    } else if j == (line_length - 1) as i16 {
+        let line_positions_to_check: [i16; 3] = [-1, 0, 1];
+        let col_positions_to_check: [i16; 2] = [-1, 0];
+
+        for k in 0..3 {
+            for l in 0..2 {
+                let line_to_check: usize = (i + line_positions_to_check[k]) as usize;
+                let col_to_check: usize = (j + col_positions_to_check[l]) as usize;
+
+                let char_to_check = input[line_to_check][col_to_check];
+
+                for symbol in possible_symbols.iter() {
+                    if char_to_check == *symbol {
+                        return true;
+                    }
+                }
+            }
+        }
+        false
+    }
     else {
         let positions_to_check: [i16; 3] = [-1, 0, 1];
         for k in 0..3 {
@@ -196,6 +215,8 @@ fn get_rest_of_digit(i: usize, j: usize, input: &Vec<Vec<char>>) -> Vec<char> {
     for n in 1..3 {
         if input[i][j + n].is_digit(10) {
             rest_of_digit.push(input[i][j + n]);
+        } else {
+            return rest_of_digit
         }
     }
     rest_of_digit
